1648a0bb1c1dc920250d7e46bac244719b17518c
Brian Mellinger <bjmellin@charlotte.edu>
2026-04-30 19:42:54 -0400

    Fix: fixed testing cases

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -2,7 +2,7 @@ class conf:
     db_host = "localhost"
     db_name = "restaurant_ordering_api"
     db_port = 3306
-    db_user = "root"  #"ryan"
-    db_password = "P@ssw0rd" #"12113076"
+    db_user = "ryan"
+    db_password = "12113076"
     app_host = "localhost"
     app_port = 8000
